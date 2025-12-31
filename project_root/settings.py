f50ef6e91ee1c5d582858cfd31ed0b6e7c2e7358
touseefurehman <tousifurehman2084@gmail.com>
2025-12-31 16:35:35 +0500

    deployment

--- project_root/settings.py
+++ project_root/settings.py
@@ -204,3 +204,8 @@ cloudinary.config(secure=True, )
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.up.railway.app',
+]
